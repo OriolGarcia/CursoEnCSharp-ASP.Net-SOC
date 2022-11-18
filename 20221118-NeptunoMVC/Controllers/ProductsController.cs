869d926358dc7575f08a84fb1d0197208fec4e82
OriolGarcia <ogs1017@gmail.com>
2022-11-18 12:48:38 +0100

    modulo 2 acabado.Empezando modulo 3

--- 20221118-NeptunoMVC/Controllers/ProductsController.cs
+++ 20221118-NeptunoMVC/Controllers/ProductsController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _20221118_NeptunoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _20221118_NeptunoMVC.Controllers
 {
 	public class ProductsController : Controller
 	{
-		public IActionResult Index()
+		public IActionResult Index(int CategoryId=0)
 		{
+
+			ProductsModel pm = new ProductsModel();
+			var lista = pm.GetProducts(CategoryId);
+			ViewData["products"]=lista;
+
 			return View();
 		}
 	}
---
+++ 20221118-NeptunoMVC/Models/ProductsModel.cs
@@ -0,0 +1,35 @@
+﻿using _20221118_NeptunoMVC.Dal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20221118_NeptunoMVC.Models
+{
+	public class ProductsModel
+	{
+		public List<ProductSlim> GetProducts(int CategoryId)
+		{
+            var dbContext = new cifo_OGSContext();
+            var products= dbContext.Products.Where(p=>p.CategoryId == CategoryId)
+				.OrderBy(p=>p.ProductName).Select(p=>new ProductSlim{
+				ProductId=p.ProductId,
+				ProductName=p.ProductName,
+				CategoryName=p.Category.CategoryName,
+				Price=p.UnitPrice,
+				Stock=p.UnitsInStock,
+				State=(p.Discontinued==0)?true:false,
+			}).ToList();
+			return products;
+
+        }
+	}
+	public class ProductSlim
+	{
+		public int ProductId { get; set; }
+		public string ProductName { get; set; }
+		public string CategoryName { get; set; }
+		public decimal? Price { get; set; }
+		public short? Stock { get; set; }
+		public bool State { get; set; }
+
+	}
+}
